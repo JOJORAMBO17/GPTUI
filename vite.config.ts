f9ef089bc0e4439a0a21e5ac45e36a0cbc599080
ramboma <rambo@apjcorp.com>
2023-09-05 10:35:25 +0800

    add config && tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import UnoCSS from 'unocss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import UnoCSS from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "gptui",
+  },
   plugins: [vue(), UnoCSS()],
-})
+});
